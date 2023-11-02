7cb1c072620d1573fe01045ee59558f17f7826fe
Yonatan Boehm <yonatanboehm@Yonatans-MacBook-Pro.local>
2023-11-02 22:31:11 +0200

    exercise 2.1

---
+++ part2/courseinfo2.1/src/components/Course.jsx
@@ -0,0 +1,11 @@
+const Course = ({ name, exercises }) => {
+    return (
+        <div>
+            <p>
+                {name} {exercises}
+            </p>
+        </div>
+    )
+}
+
+export default Course
