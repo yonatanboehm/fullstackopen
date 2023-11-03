042b7f19d40c8cb0bf7e9ffdc1cd9e175ebfe774
Yonatan Boehm <yonatanboehm@Yonatans-MacBook-Pro.local>
2023-11-03 21:14:25 +0200

    exercise 2.9

--- part2/phonebook2.6/src/App.jsx
+++ part2/phonebook2.6/src/App.jsx
@@ -1,42 +1,74 @@
 import { useState } from 'react'
 
-const Name = ({ name }) => {
+const Name = ({ name, number }) => {
   return (
-    <li>{name}</li>
+    <li>{name} {number}</li>
   )
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearch = (event) => {
+    setNewSearch(event.target.value)
+  }
+
+  const searchedNames = newSearch == '' ? persons : persons.filter(person => person.name.includes(newSearch))
+
   const addName = (event) => {
     event.preventDefault()
-    const nameObject = {
+    const personObject = {
       name: newName,
+      number: newNumber
     }
     if (persons.map(person => person.name).includes(newName)) {
       alert(`${newName} is already added to phonebook`)
       return
     }
-    setPersons(persons.concat(nameObject))
+    setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input 
+          value={newSearch}
+          autoFocus
+          onChange={handleSearch}
+          placeholder='Search name' />
+      </div>
+      <h2>Add a new:</h2>
       <form onSubmit={addName}>
         <div>
           name: <input 
           value={newName}
-          autoFocus
           onChange={handleNameChange}
           placeholder='Enter name' />
+        </div>
+        <div>
+          number: <input 
+          value={newNumber}
+          onChange={handleNumberChange}
+          placeholder='Enter number' />
         </div>
         <div>
           <button type="submit">add</button>
@@ -44,8 +76,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
-          <Name key={person.name} name={person.name}/>
+        {searchedNames.map(person =>
+            <Name key={person.name} name={person.name} number={person.number}/>
         )}
       </ul>
     </div>
