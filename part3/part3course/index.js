ed22caaec7f41e385ca2aee3c3d842a8efdd1c5a
Yonatan Boehm <yonatanboehm@Yonatans-MacBook-Pro.local>
2023-11-13 20:04:26 +0200

    exercises 3.15 - 3.18 frontend fillers

--- part3/part3course/index.js
+++ part3/part3course/index.js
@@ -2,9 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-const cors = require('cors')
-
-app.use(cors())
 app.use(express.static('dist'))
 
 app.use(express.json())
@@ -20,25 +17,25 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -57,17 +54,35 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.put('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  const changedNote = { ...note, important: !note.important }
-  notes = notes.map(note => note.id !== id ? note : changedNote)
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+  console.log(body)
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
 
-  response.json(changedNote)
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+
   
